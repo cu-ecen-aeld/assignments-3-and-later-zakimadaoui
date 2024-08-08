26bbb6207bf996eba6925a067cb3f2b1706f7b70
Zakaria Madaoui <zakimadaoui027@gmail.com>
2024-08-08 12:45:07 +0300

    assingment2 submission

---
+++ finder-app/writer.c
@@ -0,0 +1,36 @@
+#include <errno.h>
+#include <stdio.h>
+#include <syslog.h>
+
+int main(int argc, char *argv[]) {
+
+  // open syslog
+  openlog("writer", 0, LOG_USER);
+
+  if (argc != 3) {
+    printf("Usage: %s <writefile> <writestr>\n", argv[0]);
+    syslog(LOG_ERR, "Wrong arguments provided");
+    return 1;
+  }
+
+  char *write_path = argv[1];
+  char *write_text = argv[2];
+  FILE *f = fopen(write_path, "w");
+  if (!f) {
+    syslog(LOG_ERR, "could not open file %s", write_path);
+    return -1;
+  }
+
+  syslog(LOG_DEBUG, "Writing %s to %s", write_text, write_text);
+  if (!fputs(write_text, f)) {
+    syslog(LOG_ERR, "could write to file %s. Errno %d.", write_path, errno);
+  }
+  if (!fputc('\n', f)) {
+    syslog(LOG_ERR, "could write to file %s. Errno %d.", write_path, errno);
+  }
+  
+  // do not forget to close the file
+  fclose(f);
+
+  return 0;
+}
