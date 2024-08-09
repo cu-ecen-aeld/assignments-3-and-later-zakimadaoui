300ec3a4905371f8a2c205f4875775ecb58c79b3
Zakaria Madaoui <zakimadaoui027@gmail.com>
2024-08-09 16:18:23 +0300

    assignment 3 part 1 submission

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wait.h>
+#include <errno.h>
 
 bool do_system(const char *command);
 
